6172482d1f188a4ba2befe21f84546b989ccd7f0
Miguel Beltran <ingmiguelbeltran1826@gmail.com>
2026-03-03 21:37:31 -0500

    creacion de entidades y clientes

--- BackofficeAPI/Entities/Client..cs
+++ BackofficeAPI/Entities/Client..cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public string Nombre { get; set; } = string.Empty;
+        public string apellido { get; set; } = string.Empty;
         public string Telefono { get; set; } = string.Empty;
         public string Direccion { get; set; } = string.Empty;
 
