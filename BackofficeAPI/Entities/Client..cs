f0a9405ed5fd74327e7a655552aa68e11be7dee3
ESTUDIANTES <ESTUDIANTES@101-16>
2026-03-03 21:31:53 -0500

    Agregar archivos de proyecto.

---
+++ BackofficeAPI/Entities/Client..cs
@@ -0,0 +1,11 @@
+﻿namespace BackofficeAPI.Entities
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string Telefono { get; set; } = string.Empty;
+        public string Direccion { get; set; } = string.Empty;
+
+    }
+}
